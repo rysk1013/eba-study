e1e040dbd7aea9056b884caeaa48c78001d1c165
yt <brown.towel40@gmail.com>
2024-06-21 23:14:22 +0900

    jsonファイル吸い上げ処理 APIリクエスト時の有効時間判定

--- app/main.go
+++ app/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,17 +16,32 @@ import (
 
 var BaseUrl string
 var CacheRecord SiteData
+var DisplayClass DisplayClassData
+
+const api_path string = "/api/site"
+
+type DisplayClassData struct {
+	DC01 string `json:"01"`
+	DC02 string `json:"02"`
+}
 
 func init() {
 	BaseUrl, _ = os.LookupEnv("GOLANG_BACKEND_BASE_URL")
+
+	f, err := os.Open("display_class.json")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	b, _ := io.ReadAll(f)
+
+	json.Unmarshal(b, &DisplayClass)
 }
 
 func main() {
-	/*
-		@TODO
-		何かおかしい.
-		リクエストのたびにmainが実行されて変数の初期化がされる.
-	*/
+
 	e := echo.New()
 
 	e.Static("/static", "assets")
@@ -51,7 +67,13 @@ func main() {
 	e.Logger.SetLevel(utils.GetLEVEL(loglevel))
 
 	initRouting(e)
-	e.Logger.Fatal(e.Start(":1323"))
+
+	func() {
+		if err := e.Start(":1323"); err != nil {
+			e.Logger.Info(err)
+			e.Logger.Info("shutting down the server")
+		}
+	}()
 }
 
 type Template struct {
@@ -105,12 +127,14 @@ func (ac Account) GetInlineStyle() string {
 	 * @TODO
 	 * 表示の定義はjsonフォーマットで外部ファイルに切り出す.
 	 * プロセス起動時にメモリ上に配置して使用する.
+	 *
+	 * あんま意味ない、あとで戻す
 	 */
 	switch ac.DisplayClass {
 	case "01":
-		return "first"
+		return DisplayClass.DC01
 	case "02":
-		return "second"
+		return DisplayClass.DC02
 	default:
 		return ""
 	}
@@ -120,51 +144,22 @@ func index(c echo.Context) error {
 
 	e := c.Echo()
 
-	e.Logger.Info(CacheRecord)
+	var t = CacheRecord.ExpireDatetime
+	expireTime, _ := time.Parse("20060102150405", t)
 
-	url := BaseUrl + "/api/site"
+	if expireTime.Before(time.Now()) {
 
-	/*
-		@TODO
-		(c.Echo()).GET()
+		var data SiteResponse
 
-		いったん標準ライブラリで実装したがecho側で代替の機能が準備されている.
-	*/
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
+		if err := InvokeApi(&data); err != nil {
 
-		e.Logger.Error(fmt.Sprintf("Error: %s", err))
+			e.Logger.Error(fmt.Sprintf("Error: %s", err))
 
-		return err
+			return err
+		}
+
+		CacheRecord = data.SiteData
 	}
-
-	client := new(http.Client)
-
-	resp, err := client.Do(req)
-	if err != nil {
-
-		e.Logger.Error(fmt.Sprintf("Error: %s", err))
-
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	byteArray, _ := io.ReadAll(resp.Body)
-
-	var data SiteResponse
-
-	if err := json.Unmarshal(byteArray, &data); err != nil {
-		logger := c.Logger()
-		mes := fmt.Sprintf("Error: %s", err)
-		logger.Error(mes)
-
-		return err
-	}
-
-	e.Logger.Info(data.SiteData)
-
-	CacheRecord = data.SiteData
 
 	/*
 		for _, v := range data.SiteData.Accounts {
@@ -180,4 +175,34 @@ func index(c echo.Context) error {
 	}
 
 	return c.Render(http.StatusOK, "index", content)
+}
+
+func InvokeApi(data *SiteResponse) error {
+
+	url := BaseUrl + api_path
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+
+		return err
+	}
+
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+	if err != nil {
+
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	byteArray, _ := io.ReadAll(resp.Body)
+
+	if err := json.Unmarshal(byteArray, &data); err != nil {
+
+		return err
+	}
+
+	return nil
 }
